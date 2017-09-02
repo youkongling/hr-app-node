91d40dae739b9bd4fbbd6d4f285807880bb9c59a
xiaobo <1968468227@qq.com>
2017-09-02 21:34:00 +0800

    展示页面完成

---
+++ public/javascripts/app.js
@@ -0,0 +1,39 @@
+$('.swiper-slide,.swiper-container').css({
+    height: $(window).height()
+});
+
+$(window).on('resize', function() {
+    $('.swiper-slide,.swiper-container').css({
+        height: $(window).height()
+    });
+});
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    initialSlide: 0,
+    setWrapperSize: true,
+    direction: "horizontal",
+    speed: 1000,
+    longSwipesRatio: 0.3,
+    grabCursor: true,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    mousewheelControl: true,
+    lazyLoading: true,
+    lazyLoadingInPrevNext: true,
+    lazyLoadingOnTransitionStart: true,
+    onInit: function(swiper) {
+        setTimeout(function() {
+            $("#loading").remove();
+            $(".swiper-container").css({ "opacity": "1" });
+            swiperAnimateCache(swiper);
+            swiperAnimate(swiper);
+        }, 1000);
+    },
+    onSlideChangeEnd: function(swiper) {
+        swiperAnimate(swiper);
+    },
+    onTransitionEnd: function(swiper) {
+        swiperAnimate(swiper);
+    }
+});
