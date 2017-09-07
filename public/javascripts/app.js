767dadf09b5a7594a93df4bd893331db44275116
xiaobo <1968468227@qq.com>
2017-09-07 14:06:43 +0800

    增加报名表单页面

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -38,8 +38,50 @@ var mySwiper = new Swiper('.swiper-container', {
     }
 });
 
-$(document).ready(function(){
-    document.addEventListener("touchstart",function(){
+$(document).ready(function() {
+    document.addEventListener("touchstart", function() {
         document.getElementById('bgMusic').play();
     })
+})
+
+var pagination = document.querySelector('.swiper-pagination');
+
+var slide2 = document.querySelector('.swiper-slide2');
+var slide2_sign = slide2.querySelector('.sign');
+var slide2_page = slide2.querySelector('.page');
+var slide2_page_joinBtn = slide2.querySelector('.slide2-join');
+slide2_page_joinBtn.addEventListener("touchmove", function() {
+    slide2_sign.classList.add('sign-down');
+    slide2_page.classList.add('page-down');
+    pagination.classList.add('pagination-hide');
+})
+
+var slide3 = document.querySelector('.swiper-slide3');
+var slide3_sign = slide3.querySelector('.sign');
+var slide3_page = slide3.querySelector('.page');
+var slide3_page_joinBtn = slide3.querySelector('.slide3-join');
+slide3_page_joinBtn.addEventListener("touchmove", function() {
+    slide3_sign.classList.add('sign-down');
+    slide3_page.classList.add('page-down');
+    pagination.classList.add('pagination-hide');
+})
+
+var slide4 = document.querySelector('.swiper-slide4');
+var slide4_sign = slide4.querySelector('.sign');
+var slide4_page = slide4.querySelector('.page');
+var slide4_page_joinBtn = slide4.querySelector('.slide4-join');
+slide4_page_joinBtn.addEventListener("touchmove", function() {
+    slide4_sign.classList.add('sign-down');
+    slide4_page.classList.add('page-down');
+    pagination.classList.add('pagination-hide');
+})
+
+var slide5 = document.querySelector('.swiper-slide5');
+var slide5_sign = slide5.querySelector('.sign');
+var slide5_page = slide5.querySelector('.page');
+var slide5_page_joinBtn = slide5.querySelector('.slide5-join');
+slide5_page_joinBtn.addEventListener("touchmove", function() {
+    slide5_sign.classList.add('sign-down');
+    slide5_page.classList.add('page-down');
+    pagination.classList.add('pagination-hide');
 })
